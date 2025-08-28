d00375c9d5bb3df0c879f93a40a8c75c3e072d87
Caden Thompson <110309790+Nedac23@users.noreply.github.com>
2025-08-28 13:53:28 -0500

    Add files via upload

---
+++ HW4-SIFT.py
@@ -0,0 +1,80 @@
+import matplotlib.pyplot as plt
+import cv2
+from skimage import data
+from skimage import transform
+from skimage.color import rgb2gray
+from skimage.feature import match_descriptors, plot_matched_features, SIFT
+#https://scikit-image.org/docs/0.25.x/auto_examples/features_detection/plot_sift.html
+imagepath = 'image path'
+img1 = cv2.imread(imagepath, 0)
+img2 = transform.rotate(img1, 180)
+tform = transform.AffineTransform(scale=(1.3, 1.1), rotation=0.5, translation=(0, -200))
+img3 = transform.warp(img1, tform)
+descriptor_extractor = SIFT()
+descriptor_extractor.detect_and_extract(img1)
+keypoints1 = descriptor_extractor.keypoints
+descriptors1 = descriptor_extractor.descriptors
+descriptor_extractor.detect_and_extract(img2)
+keypoints2 = descriptor_extractor.keypoints
+descriptors2 = descriptor_extractor.descriptors
+descriptor_extractor.detect_and_extract(img3)
+keypoints3 = descriptor_extractor.keypoints
+descriptors3 = descriptor_extractor.descriptors
+matches12 = match_descriptors(
+    descriptors1, descriptors2, max_ratio=0.6, cross_check=True
+)
+matches13 = match_descriptors(
+    descriptors1, descriptors3, max_ratio=0.6, cross_check=True
+)
+fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(11, 8))
+plt.gray()
+plot_matched_features(
+    img1,
+    img2,
+    keypoints0=keypoints1,
+    keypoints1=keypoints2,
+    matches=matches12,
+    ax=ax[0, 0],
+)
+ax[0, 0].axis('off')
+ax[0, 0].set_title("Original Image vs. Flipped Image\n" "(all keypoints and matches)")
+plot_matched_features(
+    img1,
+    img3,
+    keypoints0=keypoints1,
+    keypoints1=keypoints3,
+    matches=matches13,
+    ax=ax[1, 0],
+)
+ax[1, 0].axis('off')
+ax[1, 0].set_title(
+    "Original Image vs. Transformed Image\n" "(all keypoints and matches)"
+)
+plot_matched_features(
+    img1,
+    img2,
+    keypoints0=keypoints1,
+    keypoints1=keypoints2,
+    matches=matches12[::15],
+    ax=ax[0, 1],
+    only_matches=True,
+)
+ax[0, 1].axis('off')
+ax[0, 1].set_title(
+    "Original Image vs. Flipped Image\n" "(subset of matches for visibility)"
+)
+plot_matched_features(
+    img1,
+    img3,
+    keypoints0=keypoints1,
+    keypoints1=keypoints3,
+    matches=matches13[::15],
+    ax=ax[1, 1],
+    only_matches=True,
+)
+ax[1, 1].axis('off')
+ax[1, 1].set_title(
+    "Original Image vs. Transformed Image\n" "(subset of matches for visibility)"
+)
+plt.tight_layout()
+plt.show()
